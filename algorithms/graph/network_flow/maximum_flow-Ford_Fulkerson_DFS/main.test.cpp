6f1d48a7e879c519a6803479c585c149867eca11
Adam Pelc <adam.pelc@adampelc.com>
2025-03-06 23:20:26 +0100

    Update Max flow

--- algorithms/graph/network_flow/maximum_flow-Ford_Fulkerson_DFS/main.test.cpp
+++ algorithms/graph/network_flow/maximum_flow-Ford_Fulkerson_DFS/main.test.cpp
@@ -6,13 +6,13 @@ TEST(Maximum_Flow_Ford_Fulkerson_DFS, Scenario_1)
 {
     // Arrange
     std::array edges = {
-            edge_t('s', 0, 10),
-            edge_t('s', 1, 10),
-            edge_t(0, 2, 25),
-            edge_t(1, 3, 15),
-            edge_t(2, 't', 10),
-            edge_t(3, 0, 6),
-            edge_t(3, 't', 10),
+            edge_t(source_node_t('s'), destination_node_t(0), capacity_t(10)),
+            edge_t(source_node_t('s'), destination_node_t(1), capacity_t(10)),
+            edge_t(source_node_t(0), destination_node_t(2), capacity_t(25)),
+            edge_t(source_node_t(1), destination_node_t(3), capacity_t(15)),
+            edge_t(source_node_t(2), destination_node_t('t'), capacity_t(10)),
+            edge_t(source_node_t(3), destination_node_t(0), capacity_t(6)),
+            edge_t(source_node_t(3), destination_node_t('t'), capacity_t(10)),
     };
 
     ford_fulkerson_dfs_solver_t solver;
@@ -23,7 +23,7 @@ TEST(Maximum_Flow_Ford_Fulkerson_DFS, Scenario_1)
     }
 
     // Act
-    const auto actual_max_flow = solver.get_max_flow('s', 't');
+    const auto actual_max_flow = solver.get_max_flow(source_node_t('s'), destination_node_t('t'));
 
     ASSERT_EQ(actual_max_flow, 20);
 }
@@ -32,23 +32,23 @@ TEST(Maximum_Flow_Ford_Fulkerson_DFS, Scenario_2)
 {
     // Arrange
     std::array edges = {
-        edge_t('s', 0, 10),
-        edge_t('s', 1, 5),
-        edge_t('s', 2, 10),
-        edge_t(0, 3, 10),
-        edge_t(1, 2, 10),
-        edge_t(2, 5, 15),
-        edge_t(3, 1, 20),
-        edge_t(3, 6, 15),
-        edge_t(4, 1, 15),
-        edge_t(4, 3, 3),
-        edge_t(5, 4, 4),
-        edge_t(5, 8, 10),
-        edge_t(6, 7, 10),
-        edge_t(6, 't', 15),
-        edge_t(7, 4, 10),
-        edge_t(7, 5, 7),
-        edge_t(8, 't', 10),
+        edge_t(source_node_t('s'), destination_node_t(0), capacity_t(10)),
+        edge_t(source_node_t('s'), destination_node_t(1), capacity_t(5)),
+        edge_t(source_node_t('s'), destination_node_t(2), capacity_t(10)),
+        edge_t(source_node_t(0), destination_node_t(3), capacity_t(10)),
+        edge_t(source_node_t(1), destination_node_t(2), capacity_t(10)),
+        edge_t(source_node_t(2), destination_node_t(5), capacity_t(15)),
+        edge_t(source_node_t(3), destination_node_t(1), capacity_t(20)),
+        edge_t(source_node_t(3), destination_node_t(6), capacity_t(15)),
+        edge_t(source_node_t(4), destination_node_t(1), capacity_t(15)),
+        edge_t(source_node_t(4), destination_node_t(3), capacity_t(3)),
+        edge_t(source_node_t(5), destination_node_t(4), capacity_t(4)),
+        edge_t(source_node_t(5), destination_node_t(8), capacity_t(10)),
+        edge_t(source_node_t(6), destination_node_t(7), capacity_t(10)),
+        edge_t(source_node_t(6), destination_node_t('t'), capacity_t(15)),
+        edge_t(source_node_t(7), destination_node_t(4), capacity_t(10)),
+        edge_t(source_node_t(7), destination_node_t(5), capacity_t(7)),
+        edge_t(source_node_t(8), destination_node_t('t'), capacity_t(10)),
     };
 
     ford_fulkerson_dfs_solver_t solver;
@@ -59,7 +59,7 @@ TEST(Maximum_Flow_Ford_Fulkerson_DFS, Scenario_2)
     }
 
     // Act
-    const auto actual_max_flow = solver.get_max_flow('s', 't');
+    const auto actual_max_flow = solver.get_max_flow(source_node_t('s'), destination_node_t('t'));
 
     ASSERT_EQ(actual_max_flow, 23);
 }
--- algorithms/graph/network_flow/maximum_flow-Ford_Fulkerson_DFS/maximum_flow.cpp
+++ algorithms/graph/network_flow/maximum_flow-Ford_Fulkerson_DFS/maximum_flow.cpp
@@ -4,58 +4,48 @@
 #include <fmt/format.h>
 #include "maximum_flow.hpp"
 
-edge_t::edge_t(int from, int to, int capacity) : m_from(from), m_to(to), m_capacity(capacity) {}
+edge_t::edge_t(source_node_t from, destination_node_t to, capacity_t capacity) : m_from(from), m_to(to), m_capacity
+      (capacity) {}
 
-int edge_t::get_from() const {
+source_node_t edge_t::get_source() const {
     return m_from;
 }
 
-int edge_t::get_to() const {
+destination_node_t edge_t::get_destination() const {
     return m_to;
 }
 
-int edge_t::get_capacity() const {
+capacity_t edge_t::get_capacity() const {
     return m_capacity;
 }
 
-int edge_t::get_remaining_capacity() const {
-    return m_capacity - m_flow;
-}
-
-void edge_t::add_flow(int flow) {
-    if(flow > get_remaining_capacity()) {
-        throw std::invalid_argument("flow is greater than remaining capacity");
-    }
-}
-
 void ford_fulkerson_dfs_solver_t::add_edge(edge_t edge) {
-    const auto from = edge.get_from();
-    const auto to = edge.get_to();
+    const auto from = edge.get_source();
+    const auto to = edge.get_destination();
 
     // Insert information about (from) -> (to) connection
     m_graph[from].push_back(to);
     m_edges.emplace(std::make_pair(from, to), edge.get_capacity());
 
     // Insert information about (to) -> (from) residual connection
-    m_graph[to].push_back(from);
-    m_edges.emplace(std::make_pair(to, from), 0);
+    m_graph[source_node_t(to)].push_back(destination_node_t(from));
+    m_edges.emplace(std::make_pair(source_node_t(to), destination_node_t(from)), capacity_t(0));
 }
 
-std::uint64_t ford_fulkerson_dfs_solver_t::get_max_flow(int source, int sink) {
+std::uint64_t ford_fulkerson_dfs_solver_t::get_max_flow(source_node_t source, destination_node_t sink) {
+    m_sink_node = sink;
     int max_flow = 0;
 
-    std::cout << "----\n";
-    while(auto current_flow = dfs(source, sink, std::numeric_limits<int>::max())) {
+    while(auto current_flow = dfs(source, flow_t(std::numeric_limits<flow_t::value_type_t>::max()))) {
         max_flow += current_flow;
         m_visited.clear();
-        fmt::print("max_flow={}, current_flow={}\n", max_flow,current_flow);
     }
 
     return max_flow;
 }
 
-int ford_fulkerson_dfs_solver_t::dfs(int source, int sink, int flow) {
-    if(source == sink) {
+flow_t ford_fulkerson_dfs_solver_t::dfs(source_node_t source, flow_t flow) {
+    if(source.get() == m_sink_node.get()) {
         return flow;
     }
 
@@ -72,30 +62,30 @@ int ford_fulkerson_dfs_solver_t::dfs(int source, int sink, int flow) {
             continue;
         }
 
-        const auto min_flow = std::min(flow, remaining_capacity);
-        fmt::print("source={}, destination={}, remaining_capacity={}, min_flow={}\n", source, destination,
-                   remaining_capacity, min_flow);
-        const auto bottleneck = dfs(destination, sink, min_flow);
+        const auto min_flow = flow_t(std::min(flow.get(), remaining_capacity.get()));
+        const auto bottleneck = dfs(source_node_t(destination), min_flow);
 
         if(bottleneck > 0) {
             // Add flow to this
             edge.augment(bottleneck);
 
             // Add negative flow
-            auto& residual_edge = m_edges.at({destination, source});
-            residual_edge.augment(-flow);
+            auto& residual_edge = m_edges.at({source_node_t(destination), destination_node_t(source)});
+            residual_edge.augment(flow_t(-flow.get()));
             return bottleneck;
         }
     }
-    return 0;
+    return flow_t(0);
 }
 
-edge_data_t::edge_data_t(int capacity) : m_capacity(capacity) {}
+edge_data_t::edge_data_t(capacity_t capacity) : m_capacity(capacity) {}
 
-int edge_data_t::get_remaining_capacity() const {
-    return m_capacity - m_flow;
+capacity_t edge_data_t::get_remaining_capacity() const {
+    return capacity_t(m_capacity.get() - m_flow.get());
 }
 
-void edge_data_t::augment(int flow) {
-    m_flow += flow;
+void edge_data_t::augment(flow_t flow) {
+    m_flow.get() += flow;
 }
+
+
